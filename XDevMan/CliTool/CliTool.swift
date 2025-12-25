ff323bd5c65d46b98005af003e8dec1ac8325cbc
Mikhail Demidov <mike.house.nsk@gmail.com>
2025-12-25 18:38:14 +0700

    Move the cli process off main thread.

--- XDevMan/CliTool/CliTool.swift
+++ XDevMan/CliTool/CliTool.swift
@@ -7,40 +7,32 @@ enum CliTool { }
 
 extension CliTool {
     
+    @concurrent
     static func exec(_ executable: String, arguments: [String]) async throws -> String {
-        let result = Task<Result<String, MyError>, Never>.detached {
-            do {
-                await AppLogger.shared.info("\(executable) \(arguments.filter({ $0.isEmpty == false }).joined(separator: " "))")
-                let outputPipe = Pipe()
-                let errorPipe = Pipe()
-                let task = Process()
-                task.executableURL = URL(fileURLWithPath: executable)
-                task.arguments = arguments.filter({ $0.isEmpty == false })
-                task.standardOutput = outputPipe
-                task.standardError = errorPipe
-                
-                try task.run()
-                
-                let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
-                let errorData = errorPipe.fileHandleForReading.readDataToEndOfFile()
-                let output = String(decoding: outputData, as: UTF8.self)
-                let error = String(decoding: errorData, as: UTF8.self)
-                if !error.isEmpty,
-                   error != "Using Previews Device Set: '/Users/\(NSUserName())/Library/Developer/Xcode/UserData/Previews/Simulator Devices'\n" {
-                    return .failure(MyError.exec(error))
-                }
-                return .success(output)
-            } catch {
-                return .failure(MyError.run(error))
-            }
+        await AppLogger.shared.info("\(executable) \(arguments.filter({ $0.isEmpty == false }).joined(separator: " "))")
+        let outputPipe = Pipe()
+        let errorPipe = Pipe()
+        let task = Process()
+        task.executableURL = URL(fileURLWithPath: executable)
+        task.arguments = arguments.filter({ $0.isEmpty == false })
+        task.standardOutput = outputPipe
+        task.standardError = errorPipe
+
+        do {
+            try task.run()
+        } catch {
+            throw MyError.run(error)
         }
-        
-        switch await result.value {
-        case .success(let output):
-            return output
-        case .failure(let error):
-            throw error
+
+        let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
+        let errorData = errorPipe.fileHandleForReading.readDataToEndOfFile()
+        let output = String(decoding: outputData, as: UTF8.self)
+        let error = String(decoding: errorData, as: UTF8.self)
+        if !error.isEmpty,
+           error != "Using Previews Device Set: '/Users/\(NSUserName())/Library/Developer/Xcode/UserData/Previews/Simulator Devices'\n" {
+            throw MyError.exec(error)
         }
+        return output
     }
         
     enum MyError: Error {
