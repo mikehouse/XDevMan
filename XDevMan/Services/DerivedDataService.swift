afc68621177f96f77fef717b4417c9728b3e1edc
Mikhail Demidov <mike.house.nsk@gmail.com>
2026-02-23 23:31:22 +0700

    Make DerivedDataService as actor

--- XDevMan/Services/DerivedDataService.swift
+++ XDevMan/Services/DerivedDataService.swift
@@ -11,7 +11,7 @@ protocol DerivedDataServiceInterface: Sendable {
     func delete(_ app: DerivedDataApp, for ide: String) async throws
 }
 
-final class DerivedDataService: DerivedDataServiceInterface {
+actor DerivedDataService: DerivedDataServiceInterface {
     
     let bash: BashProvider.Type
     
@@ -44,131 +44,126 @@ extension DerivedDataService {
     }
     
     func delete(_ app: DerivedDataApp, for ide: String) async throws {
-        let task = Task<Void, Swift.Error> { [self] in
+        do {
             switch ide {
             case "Fleet" where !app.name.hasSuffix("Caches"):
                 try await bash.rmDir(app.path.deletingLastPathComponent())
             default:
                 try await bash.rmDir(app.path)
             }
-        }
-        do {
-            try await task.value
         } catch {
             throw Error.deleteError(app, error)
         }
     }
-    
+
     func findDerivedData() async -> [DerivedData] {
-        let task = Task<[DerivedData], Never> {
-            let fileManager = FileManager.default
-            let xcode = DerivedData(
-                ideName: "Xcode",
-                path: URL(fileURLWithPath: "/Users/\(NSUserName())/Library/Developer/Xcode/DerivedData", isDirectory: true), apps: []
+        let fileManager = FileManager.default
+        let xcode = DerivedData(
+            ideName: "Xcode",
+            path: URL(fileURLWithPath: "/Users/\(NSUserName())/Library/Developer/Xcode/DerivedData", isDirectory: true), apps: []
+        )
+        let jbChaches = URL(fileURLWithPath: "/Users/\(NSUserName())/Library/Caches/JetBrains", isDirectory: true)
+        let googleChaches = URL(fileURLWithPath: "/Users/\(NSUserName())/Library/Caches/Google", isDirectory: true)
+        let list: [DerivedData] = ((try? fileManager.contentsOfDirectory(atPath: jbChaches.path)) ?? []).filter { name in
+            name.hasPrefix("AppCode") || name.hasPrefix("Idea") || name.hasPrefix("IntelliJIdea") || name == "Fleet"
+        }
+        .map { ide in
+            DerivedData(
+                ideName: ide,
+                path: jbChaches.appending(path: ide).appending(path: ide == "Fleet" ? "backend" : "DerivedData"), apps: []
             )
-            let jbChaches = URL(fileURLWithPath: "/Users/\(NSUserName())/Library/Caches/JetBrains", isDirectory: true)
-            let googleChaches = URL(fileURLWithPath: "/Users/\(NSUserName())/Library/Caches/Google", isDirectory: true)
-            let list: [DerivedData] = ((try? fileManager.contentsOfDirectory(atPath: jbChaches.path)) ?? []).filter { name in
-                name.hasPrefix("AppCode") || name.hasPrefix("Idea") || name.hasPrefix("IntelliJIdea") || name == "Fleet"
-            }
-                .map { ide in
-                    DerivedData(
-                        ideName: ide,
-                        path: jbChaches.appending(path: ide).appending(path: ide == "Fleet" ? "backend" : "DerivedData"), apps: []
-                    )
-                }
-                .filter({ fileManager.fileExists(atPath: $0.path.path) })
+        }
+        .filter({ fileManager.fileExists(atPath: $0.path.path) })
             + [xcode] +
-                ((try? fileManager.contentsOfDirectory(atPath: googleChaches.path)) ?? []).filter { name in
-                    name.hasPrefix("AndroidStudio")
-                }
-                .map { ide in
-                    DerivedData(
-                        ideName: ide,
-                        path: googleChaches.appending(path: ide).appending(path: "DerivedData"), apps: []
-                    )
-                }
-                .filter({ fileManager.fileExists(atPath: $0.path.path) })
-            let derDataList: [DerivedData] = list.map { derivedData in
-                let apps = ((try? fileManager.contentsOfDirectory(atPath: derivedData.path.path)) ?? [])
-                var appsList: [DerivedDataApp] = []
-                switch derivedData.ideName {
-                case "Fleet":
-                    let perApp: [[DerivedDataApp]] = apps.filter({ app in
-                        guard app.count > 21 else {
-                            return false
-                        }
-                        return app.dropFirst(app.count - 21).hasPrefix(".")
-                    })
-                        .map({ app in
-                            let dr = derivedData.path.appending(path: app).appending(path: "DerivedData")
-                            guard fileManager.fileExists(atPath: dr.path) else {
-                                return []
-                            }
-                            guard let realdDR = ((try? fileManager.contentsOfDirectory(atPath: dr.path)) ?? [])
-                                .filter({ $0.count > 29 && $0.dropFirst($0.count - 29).hasPrefix("-") }).first else {
-                                return []
-                            }
-                            let name = String(realdDR.dropLast(29))
-                            let appDerivedData = derivedData.path.appending(path: app).appending(path: "DerivedData")
-                            return [
-                                DerivedDataApp(
-                                    name: name,
-                                    path: appDerivedData.appending(path: realdDR)
-                                ),
-                                DerivedDataApp(
-                                    name: "\(name)+ModuleCaches",
-                                    path: appDerivedData.appending(path: "ModuleCache.noindex")
-                                ),
-                                DerivedDataApp(
-                                    name: "\(name)+SDKStatCaches",
-                                    path: appDerivedData.appending(path: "SDKStatCaches.noindex")
-                                ),
-                                DerivedDataApp(
-                                    name: "\(name)+SymbolCaches",
-                                    path: appDerivedData.appending(path: "SymbolCache.noindex")
-                                )
-                            ]
-                        })
-                    appsList = perApp.flatMap({ $0 }).filter({ fileManager.fileExists(atPath: $0.path.path) })
-                default:
-                    appsList = apps.filter({ app in
-                        guard app.count > 29 else {
-                            return false
-                        }
-                        return app.dropFirst(app.count - 29).hasPrefix("-")
-                    })
-                    .map({ app in
-                        DerivedDataApp(
-                            name: String(app.dropLast(29)),
-                            path: derivedData.path.appending(path: app)
-                        )
-                    })
-                    appsList += [
-                        DerivedDataApp(
-                            name: "ModuleCache*",
-                            path: derivedData.path.appending(path: "ModuleCache.noindex")
-                        ),
-                        DerivedDataApp(
-                            name: "SDKStatCaches*",
-                            path: derivedData.path.appending(path: "SDKStatCaches.noindex")
-                        ),
-                        DerivedDataApp(
-                            name: "SymbolCache*",
-                            path: derivedData.path.appending(path: "SymbolCache.noindex")
-                        )
-                    ]
-                    appsList = appsList.filter({ fileManager.fileExists(atPath: $0.path.path) })
-                }
-                return .init(
-                    ideName: derivedData.ideName,
-                    path: derivedData.path,
-                    apps: appsList
+            ((try? fileManager.contentsOfDirectory(atPath: googleChaches.path)) ?? []).filter { name in
+                name.hasPrefix("AndroidStudio")
+            }
+            .map { ide in
+                DerivedData(
+                    ideName: ide,
+                    path: googleChaches.appending(path: ide).appending(path: "DerivedData"), apps: []
                 )
             }
-            return derDataList
+            .filter({ fileManager.fileExists(atPath: $0.path.path) })
+        let derDataList: [DerivedData] = list.map { derivedData in
+            let apps = ((try? fileManager.contentsOfDirectory(atPath: derivedData.path.path)) ?? [])
+            var appsList: [DerivedDataApp] = []
+            switch derivedData.ideName {
+            case "Fleet":
+                let perApp: [[DerivedDataApp]] = apps.filter({ app in
+                    guard app.count > 21 else {
+                        return false
+                    }
+                    return app.dropFirst(app.count - 21).hasPrefix(".")
+                })
+                .map({ app in
+                    let dr = derivedData.path.appending(path: app).appending(path: "DerivedData")
+                    guard fileManager.fileExists(atPath: dr.path) else {
+                        return []
+                    }
+                    guard let realDR = ((try? fileManager.contentsOfDirectory(atPath: dr.path)) ?? [])
+                        .filter({ $0.count > 29 && $0.dropFirst($0.count - 29).hasPrefix("-") }).first
+                    else {
+                        return []
+                    }
+                    let name = String(realDR.dropLast(29))
+                    let appDerivedData = derivedData.path.appending(path: app).appending(path: "DerivedData")
+                    return [
+                        DerivedDataApp(
+                            name: name,
+                            path: appDerivedData.appending(path: realDR)
+                        ),
+                        DerivedDataApp(
+                            name: "\(name)+ModuleCaches",
+                            path: appDerivedData.appending(path: "ModuleCache.noindex")
+                        ),
+                        DerivedDataApp(
+                            name: "\(name)+SDKStatCaches",
+                            path: appDerivedData.appending(path: "SDKStatCaches.noindex")
+                        ),
+                        DerivedDataApp(
+                            name: "\(name)+SymbolCaches",
+                            path: appDerivedData.appending(path: "SymbolCache.noindex")
+                        )
+                    ]
+                })
+                appsList = perApp.flatMap({ $0 }).filter({ fileManager.fileExists(atPath: $0.path.path) })
+            default:
+                appsList = apps.filter({ app in
+                    guard app.count > 29 else {
+                        return false
+                    }
+                    return app.dropFirst(app.count - 29).hasPrefix("-")
+                })
+                .map({ app in
+                    DerivedDataApp(
+                        name: String(app.dropLast(29)),
+                        path: derivedData.path.appending(path: app)
+                    )
+                })
+                appsList += [
+                    DerivedDataApp(
+                        name: "ModuleCache*",
+                        path: derivedData.path.appending(path: "ModuleCache.noindex")
+                    ),
+                    DerivedDataApp(
+                        name: "SDKStatCaches*",
+                        path: derivedData.path.appending(path: "SDKStatCaches.noindex")
+                    ),
+                    DerivedDataApp(
+                        name: "SymbolCache*",
+                        path: derivedData.path.appending(path: "SymbolCache.noindex")
+                    )
+                ]
+                appsList = appsList.filter({ fileManager.fileExists(atPath: $0.path.path) })
+            }
+            return .init(
+                ideName: derivedData.ideName,
+                path: derivedData.path,
+                apps: appsList
+            )
         }
-        return await task.value
+        return derDataList
     }
 }
 
